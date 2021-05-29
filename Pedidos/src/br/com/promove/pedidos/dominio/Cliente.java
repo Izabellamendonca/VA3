eaa65b4abbaa6cf99d3463389fee526188a84210
Rediner Alves Vinhal <rediner@hotmail.com>
2021-05-29 14:59:56 -0300

    construtor cliente

--- Pedidos/src/br/com/promove/pedidos/dominio/Cliente.java
+++ Pedidos/src/br/com/promove/pedidos/dominio/Cliente.java
@@ -2,7 +2,14 @@ package br.com.promove.pedidos.dominio;
 
 public class Cliente {
 	
-
+	public Cliente(int codigo, String nome, String fone, String email, boolean ativo) {
+		super();
+		this.codigo = codigo;
+		this.nome = nome;
+		this.fone = fone;
+		this.email = email;
+		this.ativo = ativo;
+	}
 	
 	private int codigo;
 	private String nome;
