15a7fd324f27f4bf2863ce0299eafcb579461c7e
Rediner Alves Vinhal <rediner@hotmail.com>
2021-05-29 15:18:57 -0300

    gets and sets

--- Pedidos/src/br/com/promove/pedidos/dominio/Cliente.java
+++ Pedidos/src/br/com/promove/pedidos/dominio/Cliente.java
@@ -20,4 +20,45 @@ public class Cliente {
 	private String fone;
 	private String email;
 	private boolean ativo;
+
+	public int getCodigo() {
+		return codigo;
+	}
+
+	public void setCodigo(int codigo) {
+		this.codigo = codigo;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getFone() {
+		return fone;
+	}
+
+	public void setFone(String fone) {
+		this.fone = fone;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public boolean isAtivo() {
+		return ativo;
+	}
+
+	public void setAtivo(boolean ativo) {
+		this.ativo = ativo;
+	}
+	
 }
